d4629b2b94d0c73866ca604270ca16ae5de7683e
calebchiam <calebchiam@gmail.com>
2021-09-27 02:27:57 -0400

    expanded tests

--- convokit/tests/general/test_from_pandas.py
+++ convokit/tests/general/test_from_pandas.py
@@ -5,20 +5,28 @@ from convokit import download
 class CorpusFromPandas(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.corpus = Corpus(download('subreddit-Cornell'))
-
-    def test_reconstruction(self):
-        """
-        Test that reconstructing the Corpus from outputted dataframes results in the same number of corpus components
-        """
-
+        self.corpus = Corpus(download('subreddit-hey'))
         utt_df = self.corpus.get_utterances_dataframe()
         convo_df = self.corpus.get_conversations_dataframe()
         speaker_df = self.corpus.get_speakers_dataframe()
-        new_corpus = Corpus.from_pandas(speaker_df, utt_df, convo_df)
-        assert len(new_corpus.speakers) == len(self.corpus.speakers)
-        assert len(new_corpus.conversations) == len(self.corpus.conversations)
-        assert len(new_corpus.utterances) == len(self.corpus.utterances)
+        self.new_corpus = Corpus.from_pandas(speaker_df, utt_df, convo_df)
+
+    def test_reconstruction_stats(self):
+        """
+        Test that reconstructing the Corpus from outputted dataframes results in the same number of corpus components
+        """
+        assert len(self.new_corpus.speakers) == len(self.corpus.speakers)
+        assert len(self.new_corpus.conversations) == len(self.corpus.conversations)
+        assert len(self.new_corpus.utterances) == len(self.corpus.utterances)
+
+    def test_reconstruction_metadata(self):
+        assert set(self.corpus.random_utterance().meta) == set(self.new_corpus.random_utterance().meta)
+        assert set(self.corpus.random_conversation().meta) == set(self.new_corpus.random_conversation().meta)
+        assert set(self.corpus.random_speaker().meta) == set(self.new_corpus.random_speaker().meta)
+
+    def test_convo_reconstruction(self):
+        for convo in self.new_corpus.iter_conversations():
+            assert convo.check_integrity(verbose=False)
 
 if __name__ == '__main__':
     unittest.main()
