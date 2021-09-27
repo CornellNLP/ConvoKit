34109affa68eb31a719edac6587df901f5c24d0c
calebchiam <calebchiam@gmail.com>
2021-09-27 01:36:52 -0400

    Update test_from_pandas.py

--- convokit/tests/general/test_from_pandas.py
+++ convokit/tests/general/test_from_pandas.py
@@ -9,16 +9,16 @@ class CorpusFromPandas(unittest.TestCase):
 
     def test_reconstruction(self):
         """
-        Test basic meta functions
+        Test that reconstructing the Corpus from outputted dataframes results in the same number of corpus components
         """
 
         utt_df = self.corpus.get_utterances_dataframe()
         convo_df = self.corpus.get_conversations_dataframe()
         speaker_df = self.corpus.get_speakers_dataframe()
-        print(utt_df.columns)
         new_corpus = Corpus.from_pandas(speaker_df, utt_df, convo_df)
-        self.corpus.print_summary_stats()
-        new_corpus.print_summary_stats()
+        assert len(new_corpus.speakers) == len(self.corpus.speakers)
+        assert len(new_corpus.conversations) == len(self.corpus.conversations)
+        assert len(new_corpus.utterances) == len(self.corpus.utterances)
 
 if __name__ == '__main__':
     unittest.main()
