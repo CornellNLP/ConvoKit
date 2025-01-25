0594315d69475058de35e20c5d7a93ab3844c845
ex36 <96800594+Torchee@users.noreply.github.com>
2025-01-24 21:52:52 -0500

    Revert "making spacy optional"
    This reverts commit be6a7c6bc5ea96c8003020b6698ed9a1afe18683.

--- examples/politeness-strategies/awry_politeness_strategies.py
+++ examples/politeness-strategies/awry_politeness_strategies.py
@@ -4,42 +4,21 @@
 #   `conversations-gone-awry` example subdirectory.
 
 import pandas as pd
-# from .model import *
-#     from .util import *
-#     from .coordination import *
-#     from .transformer import *
-#     from .convokitPipeline import *
-#     from .hyperconvo import *
-#     from .speakerConvoDiversity import *
-#     from .phrasing_motifs import *
-#     from .prompt_types import *
-#     from .classifier import *
-#     from .ranker import *
-#     from .forecaster import *
-#     from .fighting_words import *
-#     from .paired_prediction import *
-#     from .bag_of_words import *
-#     from .expected_context_framework import *
-#     from .surprise import *
-#     from .convokitConfig import *
-import convokit
-from convokit import Transformer, ConvokitPipeline, HyperConvo, SpeakerConvoDiversity, Classifier, Ranker, Forecaster, FightingWords, PairedPrediction, bag_of_words, surprise, convokitConfig
-from convokit import phrasing_motifs
-from convokit import text_processing
-from convokit import politenessStrategies
-# print("Loading awry corpus...")
-# corpus = Corpus(filename=download("conversations-gone-awry-corpus"))
+from convokit import PolitenessStrategies, Corpus, download
 
-# # extract the politeness strategies.
-# # Note: politeness strategies are a hand-engineered feature set, so no fitting is needed.
-# ps = PolitenessStrategies(verbose=100)
-# print("Extracting politeness strategies...")
-# corpus = ps.transform(corpus)
+print("Loading awry corpus...")
+corpus = Corpus(filename=download("conversations-gone-awry-corpus"))
 
-# values = []
-# idx = []
-# for utterance in corpus.iter_utterances():
-#     values.append(utterance.meta["politeness_strategies"])
-#     idx.append(utterance.id)
-# pd.DataFrame(values, index=idx).to_csv("awry_strategy_df_v2.csv")
-# print("Done, results written to awry_strategy_df_v2.csv")
+# extract the politeness strategies.
+# Note: politeness strategies are a hand-engineered feature set, so no fitting is needed.
+ps = PolitenessStrategies(verbose=100)
+print("Extracting politeness strategies...")
+corpus = ps.transform(corpus)
+
+values = []
+idx = []
+for utterance in corpus.iter_utterances():
+    values.append(utterance.meta["politeness_strategies"])
+    idx.append(utterance.id)
+pd.DataFrame(values, index=idx).to_csv("awry_strategy_df_v2.csv")
+print("Done, results written to awry_strategy_df_v2.csv")
