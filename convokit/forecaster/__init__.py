0218e985d80e16a0882ad53095963b0515b88879
Son Quoc Tran <sqt2@cornell.edu>
2025-06-16 19:32:43 +0000

    Update Decoder demo

--- convokit/forecaster/__init__.py
+++ convokit/forecaster/__init__.py
@@ -5,7 +5,6 @@ import sys
 
 if "torch" in sys.modules:
     from .CRAFTModel import *
-    from .BERTCGAModel import *
     from .CRAFT import *
     from .TransformerEncoderModel import *
     from .TransformerDecoderModel import *
