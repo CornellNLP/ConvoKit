3a6001955bc7f2580f24c9b2581a59156b42d4e8
seanzhangkx8 <106214464+seanzhangkx8@users.noreply.github.com>
2025-11-03 08:52:21 -0500

    ConvoKit Version 4.0.0 (#325)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="cristian@cs.cornell.edu",
     url="https://github.com/CornellNLP/ConvoKit",
     description="ConvoKit",
-    version="3.6.0",
+    version="4.0.0",
     packages=[
         "convokit",
         "convokit.bag_of_words",
