e276931195e9330e84817704e59793cef029a953
seanzhangkx8 <106214464+seanzhangkx8@users.noreply.github.com>
2026-05-01 12:24:35 -0400

    Version increment 4.1.1

--- website/docs/source/conf.py
+++ website/docs/source/conf.py
@@ -8,9 +8,9 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 project = "ConvoKit"
-copyright = "2025, Cornell NLP"
+copyright = "2026, Cornell NLP"
 author = "Cornell NLP"
-release = "4.1.0"
+release = "4.1.1"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
